ea545964202e853e6d6b23daa32632718118de2b
pittov_20*19 <88969145+hieutmph@users.noreply.github.com>
2022-11-24 08:42:12 +0700

    Add files via upload

---
+++ khachHang.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author DO VAN DAO
+ */
+public class khachHang {
+    String maKH,tenKH,sdt,diaChi,email,queQuan,trangThai;
+    
+}
